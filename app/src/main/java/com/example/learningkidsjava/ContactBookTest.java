0cd688bac2d73e004c18cc97ce736f06fde44eb4
anniegomezz <anniegomez@my.unt.edu>
2024-04-25 01:21:14 -0500

    added test files to test buttons

--- app/src/main/java/com/example/learningkidsjava/ContactBookTest.java
+++ app/src/main/java/com/example/learningkidsjava/ContactBookTest.java
@@ -3,13 +3,12 @@ package com.example.learningkidsjava;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
 
-public class HomeScreen extends AppCompatActivity {
+public class ContactBookTest extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.home_activity);
+        setContentView(R.layout.contact_book_test);
     }
 }
--- app/src/main/java/com/example/learningkidsjava/SettingsTest.java
+++ app/src/main/java/com/example/learningkidsjava/SettingsTest.java
@@ -3,13 +3,12 @@ package com.example.learningkidsjava;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
 
-public class HomeScreen extends AppCompatActivity {
+public class SettingsTest extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.home_activity);
+        setContentView(R.layout.settings_test);
     }
 }
