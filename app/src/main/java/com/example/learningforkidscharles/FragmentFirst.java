fa481b8542e4dd21b67ff444b472dbc533bb8fb2
cml0322 <144165822+cml0322@users.noreply.github.com>
2024-04-25 22:43:00 -0500

    testing again, i really hope this doesnt break anything

---
+++ app/src/main/java/com/example/learningforkidscharles/FragmentFirst.java
@@ -0,0 +1,14 @@
+package com.example.learningforkidscharles;
+
+import android.os.Bundle;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+public class FragmentFirst {
+}
