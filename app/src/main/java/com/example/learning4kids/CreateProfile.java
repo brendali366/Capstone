9030145b28e9c89464460e7d37c93cc1ef488835
alexismatias <matias.j.alexis@gmail.com>
2024-04-27 05:24:48 -0500

    connected creating username to database

--- app/src/main/java/com/example/learning4kids/CreateProfile.java
+++ app/src/main/java/com/example/learning4kids/CreateProfile.java
@@ -1,14 +1,67 @@
 package com.example.learning4kids;
+import com.example.learning4kids.UserInfo;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class CreateProfile extends AppCompatActivity {
+
+    private EditText usernameEdt;
+    private Button createProfileButtonBtn;
+
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_profile);
+
+        // Initializing views
+        usernameEdt = findViewById(R.id.username);
+        createProfileButtonBtn = findViewById(R.id.createProfileButton);
+
+        // Initializing Firebase
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("UserInfo");
+
+        createProfileButtonBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = usernameEdt.getText().toString();
+                if (TextUtils.isEmpty(name)) {
+                    Toast.makeText(CreateProfile.this, "Please add some data.", Toast.LENGTH_SHORT).show();
+                } else {
+                    addDatatoFirebase(name);
+                }
+            }
+        });
+    }
+
+    private void addDatatoFirebase(String name) {
+
+        UserInfo userInfoObject = new UserInfo(name);
+
+        databaseReference.setValue(userInfoObject, new DatabaseReference.CompletionListener() {
+            @Override
+            public void onComplete(@NonNull DatabaseError error, @NonNull DatabaseReference ref) {
+                if (error == null) {
+                    Toast.makeText(CreateProfile.this, "Data added successfully", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(CreateProfile.this, "Fail to add data: " + error.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/learning4kids/UserInfo.java
@@ -0,0 +1,19 @@
+package com.example.learning4kids;
+
+public class UserInfo {
+    private String username;
+
+    public UserInfo() {
+
+    }
+    public UserInfo(String username) {
+        this.username = username;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+}
