b7aebbc1a5e46d12e8d8f922e3165637e735434a
Brenda Li <brendali366@gmail.com>
2024-04-26 22:32:14 -0500

    Added brown action bar and back button

--- app/src/main/java/com/example/learningkidsbrenda/SettingsPage.java
+++ app/src/main/java/com/example/learningkidsbrenda/SettingsPage.java
@@ -1,28 +1,55 @@
 package com.example.learningkidsbrenda;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.ImageView;
-
+import android.annotation.SuppressLint;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.Toolbar;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.ButtonBarLayout;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.OnApplyWindowInsetsListener;
+import java.util.Objects;
+import com.google.android.material.appbar.MaterialToolbar;
 
 
 
 public class SettingsPage extends AppCompatActivity {
+
+    private Toolbar toolbar;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.settings);
+
+        ImageView arrows = findViewById(R.id.arrow);
+        TextView title = findViewById(R.id.settingsT);
+        arrows.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SettingsPage.this, homepage.class);
+                startActivity(intent);
+            }
+        });
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
         });
     }
 }
