e6dcc07642341e24bbbca6102e41f0faa4b3ac8c
ij <ijeomachukwuma@my.unt.edu>
2024-04-30 11:37:38 -0500

    fixed small issues

--- app/src/main/java/com/example/myapplication/SettingsScreen.java
+++ app/src/main/java/com/example/myapplication/SettingsScreen.java
@@ -2,7 +2,12 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+
 
 public class SettingsScreen extends AppCompatActivity {
 
@@ -10,5 +15,18 @@ public class SettingsScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings_screen);
+
+        ImageButton settingsReturnHome;
+        settingsReturnHome = (ImageButton) findViewById(R.id.settingsReturnHome);
+
+        settingsReturnHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SettingsScreen.this, HomeScreen.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
