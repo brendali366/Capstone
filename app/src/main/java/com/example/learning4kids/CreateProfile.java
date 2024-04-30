bceffc6db298036c6cd1c2b6e9790855116234a9
alexismatias <matias.j.alexis@gmail.com>
2024-04-29 20:53:27 -0500

    editing

--- app/src/main/java/com/example/learning4kids/CreateProfile.java
+++ app/src/main/java/com/example/learning4kids/CreateProfile.java
@@ -14,6 +14,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class CreateProfile extends AppCompatActivity {
 
@@ -21,12 +22,19 @@ public class CreateProfile extends AppCompatActivity {
     private Button createProfileButtonBtn;
 
     FirebaseDatabase firebaseDatabase;
+   // FirebaseFirestore fstore; //for image
     DatabaseReference databaseReference;
+
+   // ImageView profileImage; // for image
+   // Button changeProfileImage; //for image
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_profile);
+
+       // profileImage = findViewById(R.id.profilePicture); // for image
+       // changeProfileImage = findViewById(R.id.createPFPButton); //for image
 
         // Initializing views
         usernameEdt = findViewById(R.id.username);
